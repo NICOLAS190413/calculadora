121cc5e8a7d8357b2b76c6d1e565ec403eabd061
NICOLAS190413 <145618530+NICOLAS190413@users.noreply.github.com>
2023-11-15 17:10:02 -0500

    Add files via upload

---
+++ calculadora/aseets/js/e.js
@@ -0,0 +1,31 @@
+
+function suma(){
+    var nunu=document.getElementById("unomil").value;
+    var nubu=document.getElementById("unodosmil").value;
+    nunu=parseFloat(nunu);
+    nubu=parseFloat(nubu);
+    alert(nunu+nubu)
+}
+function multiplication(){
+    var mumu=document.getElementById("unomil").value;
+    var mum=document.getElementById("unodosmil").value;
+    mumu=parseFloat(mumu);
+    mum=parseFloat(mum);
+    alert(mumu*mum)
+}
+function dividir(){
+    var tu=document.getElementById("unomil").value;
+    var yo=document.getElementById("unodosmil").value;
+    tu=parseFloat(tu);
+    yo=parseFloat(yo);
+    alert(tu/yo)
+}
+function restar(){
+    var t=document.getElementById("unomil").value;
+    var y=document.getElementById("unodosmil").value;
+    t=parseFloat(t);
+    y=parseFloat(y);
+    alert(t-y)
+}
+
+
